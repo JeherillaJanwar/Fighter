527a5b0f82b8fa921bb3d0a075dab3b4946aa97e
Ishaan Sharma <ishaan328069@gmail.com>
2022-08-25 10:34:50 -0400

    Create music.js

---
+++ objects/World/music.js
@@ -0,0 +1,75 @@
+var musics = {
+    SongList: [{
+        name: "Sea Wave Sound - Nature Sound - Beach Bums",
+        link: "https://api.soundcloud.com/tracks/200099207/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Relaxation Music Sleeping - Rain and Thunder (1 hours) - ELIRT",
+        link: "https://api.soundcloud.com/tracks/146823529/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Relaxing Rain and Loud Thunder - R.J. Stefanski",
+        link: "https://api.soundcloud.com/tracks/52301001/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Relaxing Rain and Thunder Sound , Sleep Meditation - Ibne Waheedi",
+        link: "https://api.soundcloud.com/tracks/110697958/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "30 Minute Deep Sleep Music - T Mega",
+        link: "https://api.soundcloud.com/tracks/237317520/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "RELAX – relaxdaily N°048 - relaxdaily",
+        link: "https://api.soundcloud.com/tracks/49267945/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Smooth jazz music - JWilborn",
+        link: "https://api.soundcloud.com/tracks/58735518/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "DEEP HOUSE SUMMER MIX 2 - AHMET KILIC - Ahmet Kilic",
+        link: "https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "As she passes - Levi Patel",
+        link: "https://api.soundcloud.com/tracks/191576787/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Maroon 5 - Girls Like You ft. Cardi B (Hiderway Remix)",
+        link: "https://api.soundcloud.com/tracks/455069910/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "TroyBoi - Afterhours (feat. Diplo & Nina Sky)",
+        link: "https://api.soundcloud.com/tracks/224375276/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "DJ Snake ft. Justin Bieber - Let Me Love You (Koni Remix)",
+        link: "https://api.soundcloud.com/tracks/276962559/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Cartoon - Why We Lose (feat. Coleman Trapp)",
+        link: "https://api.soundcloud.com/tracks/210195404/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Electro-Light - Symbolism",
+        link: "https://api.soundcloud.com/tracks/178912631/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Tobu - Infectious",
+        link: "https://api.soundcloud.com/tracks/155512892/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Nova - Ahrix",
+        link: "https://api.soundcloud.com/tracks/81619639/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Lensko - Cetus [NCS Release] - NCS",
+        link: "https://api.soundcloud.com/tracks/162536037/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "\"Sax On The Beach\" Mixtape Part 1",
+        link: "https://api.soundcloud.com/tracks/105110459/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Deep House Mix 2013 *Free Download* - Owen Royal",
+        link: "https://api.soundcloud.com/tracks/75853154/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Diamond Eyes - Flutter",
+        link: "https://api.soundcloud.com/tracks/515672067/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name: "Last Heroes x TwoWorldsApart - Eclipse (feat. AERYN)",
+        link: "https://api.soundcloud.com/tracks/512204838/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name:"Robin Hustin x Tobimorrow - Light It Up (feat. Jex)",
+        link:"https://api.soundcloud.com/tracks/484120113/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name:"Lost Sky - Dreams",
+        link:"https://api.soundcloud.com/tracks/477898656/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }, {
+        name:"Unknown Brain - Perfect 10 (feat. Heather Sommer)",
+        link:"https://api.soundcloud.com/tracks/470287734/stream?client_id=587aa2d384f7333a886010d5f52f302a"
+    }]
+}
